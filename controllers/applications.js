70986c4bdc69880fb3ae62618ee89b167f578703
Shazib Siddique <siddiqueshazib4@gmail.com>
2024-07-01 20:17:58 -0400

    add show page for application

--- controllers/applications.js
+++ controllers/applications.js
@@ -11,32 +11,48 @@ const User = require('../models/user.js');
 // controllers/applications.js
 
 router.get('/', async (req, res) => {
-    try {
-      res.render('applications/index.ejs');
-    } catch (error) {
-      console.log(error)
-      res.redirect('/')
-    }
-  });
-  
-  router.get('/new', (req, res) => {   
-    res.render('applications/new.ejs');
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+
+    res.render('applications/index.ejs', {
+      applications: currentUser.applications,
     });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
 
-    router.post('/', async (req, res) => {
-      try {
- 
-   const currentUser = await User.findById(req.session.user._id);
-   currentUser.applications.push(req.body);
-   await currentUser.save();
-    res.redirect(`/users/${req.session.user._id}/applications`);
-      }catch (error) {
-      console.log(error);
-      res.redirect("/");
-      }
+router.get('/new', async (req, res) => {
+  res.render('applications/new.ejs');
+});
 
-  } ); 
+router.post('/', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
 
+    currentUser.applications.push(req.body);
+
+    await currentUser.save();
+    res.redirect(`/users/${currentUser._id}/applications`);
+  } catch(error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+router.get('/:applicationId', async (req, res) => {
+  try {
+    const currentUser =  await User.findById(req.session.user._id);
+    const application = currentUser.applications.id(req.params.applicationId);
+    res.render('applications/show.ejs', {
+      application : application,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+} );
 
 module.exports = router;
 
